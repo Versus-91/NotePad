01216aa0fa567b437777ff205e7019f8843c0cee
qader qaribiyan <gharibyan.ghader@gmail.com>
2023-04-01 23:42:27 +0200

    Minimal API

--- NotePad/Program.cs
+++ NotePad/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.MapGet("/", () => "Note book Minimal API");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
