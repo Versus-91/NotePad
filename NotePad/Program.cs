3623f989f6c7a435a7d31bc5ed4aaa2679f295a1
qader qaribiyan <gharibyan.ghader@gmail.com>
2023-04-01 23:51:56 +0200

    Added In Memory Database

--- NotePad/Program.cs
+++ NotePad/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NotePad.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TestDb"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 app.MapGet("/", () => "Note book Minimal API");
 // Configure the HTTP request pipeline.
